4b8dc63b7b6b6717748a3eed096c79059dff4221
Bartosz Rosiek <brrosiek@student.wsb-nlu.edu.pl>
2024-06-13 15:58:24 +0200

    Finished

---
+++ JobBoard.Mobile/Dtos/CategoryForJobDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JobBoard.Mobile.Dtos
+{
+    public class CategoryForJobDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ JobBoard.Mobile/Dtos/CompanyCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobBoard.Mobile.Dtos
+{
+    public class CompanyCreateDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public List<int> LocationIds { get; set; } = new List<int>();
+    }
+}
